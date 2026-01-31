04597113ed90547fcaa63ae1722bc2349c382c62
huzaifa-wq <bhaihuzaifa733@gmail.com>
2026-01-31 22:47:05 +0500

    4th task

---
+++ 4th_task.py
@@ -0,0 +1,11 @@
+def sum_of_numbers(list_of_numbers):
+    total=0
+    for num in list_of_numbers:
+        total +=  number
+    return total
+
+List_of_numbers = [5,4,3,7,4]
+
+result = sum_of_numbers(List_of_numbers)
+print(result)
+
